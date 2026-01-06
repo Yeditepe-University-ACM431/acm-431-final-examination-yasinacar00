47a627eadf8f1a7f1f2f233152165cebf8effe83
yasinacar00 <yasin.acar@std.yeditepe.edu.tr>
2026-01-06 15:06:25 +0300

    Update AppNavGraph.kt

--- app/src/main/java/com/yeditepe/finalexam/navigation/AppNavGraph.kt
+++ app/src/main/java/com/yeditepe/finalexam/navigation/AppNavGraph.kt
@@ -13,9 +13,24 @@ fun AppNavGraph() {
         navController = navController,
         startDestination = "tasks"
     ) {
-
         // TODO 1: Add composable for "tasks"
+        composable("tasks") {
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text("Task List", style = MaterialTheme.typography.headlineMedium)
+                TaskRow(
+                    task = Task(1, "Final Project", false), 
+                    navController = navController
+                )
+            }
+        }
 
         // TODO 2: Add composable for "taskDetail/{title}"
+        composable(
+            route = "taskDetail/{title}",
+            arguments = listOf(navArgument("title") { type = NavType.StringType })
+        ) { backStackEntry ->
+            val taskTitle = backStackEntry.arguments?.getString("title") ?: "Unknown"
+            TaskDetailScreen(title = taskTitle)
+        }
     }
 }
